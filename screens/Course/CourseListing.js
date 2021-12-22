6d47e6751751d28e50bec5c854289b5e6f37a03f
knowsarzehmeh <osarzeh@gmail.com>
2021-12-22 12:01:01 +0100

    Animate Course Listing Header

--- screens/Course/CourseListing.js
+++ screens/Course/CourseListing.js
@@ -23,6 +23,10 @@ import { SharedElement } from 'react-navigation-shared-element'
 import { COLORS, FONTS, SIZES, icons, images, dummyData } from '../../constants'
 import { IconButton, LineDivider, HorizontalCourseCard } from '../../components'
 
+const AnimatedFlatlist = Animated.createAnimatedComponent(FlatList)
+const HEADER_HEIGHT = 250;
+
+
 function CourseListing({
     appTheme,
     route,
@@ -30,18 +34,72 @@ function CourseListing({
 }) {
 
    const { category, sharedElementPrefix } = route.params
+   const headerSharedValue = useSharedValue(80)
+   const flatListRef = React.useRef()
+   const scrollY = useSharedValue(0)
+   const onScroll = useAnimatedScrollHandler((event) => {
+       scrollY.value = event.contentOffset.y
+   })
+
+
+   function backHandler() {
+       navigation.goBack();
+   }
+
 
    function renderHeader() {
+
+        const inputRange = [ 0, HEADER_HEIGHT - 50 ]
+
+        headerSharedValue.value = withDelay(500, withTiming(0, {
+            duration: 500
+        }))
+
+        const headerFadeInAnimatedStyle = useAnimatedStyle(() => {
+            return {
+                opacity: interpolate(headerSharedValue.value, [80, 0], [0, 1])
+            }
+        })
+
+        const headerSlideInAnimatedStyle = useAnimatedStyle(() => {
+            return {
+                transform: [
+                    {
+                        translateY: headerSharedValue.value
+                    }
+                ]
+            }
+        });
+        
+        const headerHeightAnimatedStyle = useAnimatedStyle(() => {
+            return {
+                height: interpolate(scrollY.value, inputRange, [ HEADER_HEIGHT, 120 ], Extrapolate.CLAMP)
+            }
+        })
+
+        const headerHideOnScrollAnimatedStyle = useAnimatedStyle(() => {
+            return {
+                opacity: interpolate(scrollY.value, [80, 0], [ 1, 0 ], Extrapolate.CLAMP),
+                transform: [
+                    {
+                        translateY: interpolate(scrollY.value, inputRange, [200, 0], Extrapolate.CLAMP)
+                    }
+                ]
+            }
+        })
+
        return ( 
            <Animated.View
-            style={{
+            style={[{
                 position: 'absolute',
                  top: 0,
                  left: 0,
                  right: 0,
                  height: 250,
                  overflow: 'hidden'
-            }}
+            },
+            headerHeightAnimatedStyle
+        ]}
            >
                {/* Background Img */}
                <SharedElement
@@ -51,28 +109,34 @@ function CourseListing({
                    <Image
                         source={category?.thumbnail}
                         resizeMode='cover'
-                        style={{
+                        style={[{
                             height:'100%',
                             width: '100%',
                             borderBottomLeftRadius: 60,
-                        }}
+                        },
+            
+                    ]}
                    />
 
                </SharedElement>
 
                {/* Title */}
                <Animated.View
-                 style={{
+                 style={[{
                      position: 'absolute',
                      bottom: 70,
                      left: 30
-                 }}
+                 },
+                 headerHideOnScrollAnimatedStyle
+                ]}
                >
                    <SharedElement
                      id={`${sharedElementPrefix}-CategoryCard-Title-${category?.id}`}
+                     style={[StyleSheet.absoluteFillObject ]}
                    >
                        <Text
                          style={{
+                             position: 'absolute',
                              color: COLORS.white,
                              ...FONTS.h2,
                          }}
@@ -82,10 +146,128 @@ function CourseListing({
                    </SharedElement>
                      
                </Animated.View>
+
+               {/* Back Btn */}
+               <Animated.View
+                 style={headerFadeInAnimatedStyle}
+               >
+                    <IconButton 
+                     icon={icons.back}
+                     iconStyle={{
+                         tintColor: COLORS.black,
+                     }}
+                     containerStyle={{
+                         position: 'absolute',
+                         top: 40,
+                         left: 20,
+                         width: 50,
+                         height: 50,
+                         alignItems: 'center',
+                         justifyContent: 'center',
+                         borderRadius: 25,
+                         backgroundColor: COLORS.white,
+                     }}
+                     onPress={() => {
+                         backHandler();
+                     }}
+                    />
+               </Animated.View>
+
+                  {/* Category Image */}
+                  <Animated.Image
+                    source={images.mobile_image}
+                    resizeMode='contain'
+                    style={[{
+                        position: 'absolute',
+                        bottom: -40,
+                        right: 40,
+                        width: 100,
+                        height: 200
+                    },
+                    headerFadeInAnimatedStyle,
+                    headerSlideInAnimatedStyle,
+                    headerHideOnScrollAnimatedStyle,
+                ]}
+                    />
+
            </Animated.View>
            )
        
    }
+
+
+   function renderResults() {
+       
+    return (
+        <AnimatedFlatlist
+            ref={flatListRef}
+            data={dummyData.courses_list_2}
+            keyExtractor={item => `Results-${item.id}`}
+            contentContainerStyle={{
+                paddingHorizontal: SIZES.padding,
+
+            }}
+            showsHorizontalScrollIndicator={false}
+            scrollEventThrottle={16}
+            keyboardDismissMode="on-drag"
+            onScroll={onScroll}
+            ListHeaderComponent={() => {
+                return (
+                    <View
+                      style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                          marginTop: 270,
+                          marginBottom: SIZES.base,
+                      }}
+                     >
+                         {/* Results */}
+                         <Text
+                          style={{
+                              flex: 1,
+                              ...FONTS.body3,
+                          }}
+                         >
+                             5,767 Results
+                         </Text>
+
+                         {/* Filter Btn */}
+                         <IconButton 
+                            icon={icons.filter}
+                            iconStyle={{
+                                width: 20,
+                                height: 20
+                            }}
+                            containerStyle={{
+                                width: 40,
+                                height: 40,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                borderRadius: 10,
+                                backgroundColor: COLORS.primary
+                            }}
+                         />
+                    </View>
+                )
+            }}
+
+            renderItem={({item, index}) => {
+                return (
+                    <HorizontalCourseCard 
+                     course={item}
+                     containerStyle={{
+                         marginVertical: SIZES.padding,
+                         marginTop: index === 0 ? SIZES.radius : SIZES.padding
+                     }}
+                    />
+                )
+            }}
+            
+          ItemSeparatorComponent={() => <LineDivider lineStyle={{ height: 1, backgroundColor: COLORS.gray20 }} />}
+        />
+    );
+   }
+
 
 
     return (
@@ -94,6 +276,10 @@ function CourseListing({
              flex: 1
          }}
         >
+            {/* Results */}
+            {renderResults()}
+
+            {/* Header */}
             {renderHeader()}
         </View>
     )
